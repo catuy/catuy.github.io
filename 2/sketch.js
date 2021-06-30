4aacd54bbcb04c23bea6fdabcb86d846b5c235f2
catuy <cataldo.diego@gmail.com>
2021-06-30 01:45:31 -0300

    .

--- 2/sketch.js
+++ 2/sketch.js
@@ -1,51 +1,36 @@
-var dim;
-
-function setup() {
-  createCanvas(displayWidth, displayHeight);
-  let fill_color = color('#312783');
-  let stroke_color = color('#E6007E');
-  let background_color = color('#eee');
-  let stroke_wight = 0;
-  dim = width/2;
-  colorMode(HSB, 360, 100, 100);
-  ellipseMode(RADIUS);
-
-
-  background(background_color);
-  fill(fill_color);
-  stroke(stroke_color);
-  strokeWeight(stroke_wight);
-
-}
-
-function draw() {
-  // background(0);
-  for (var x = 0; x <= width; x+=dim) {
-    drawGradient(x, height/2);
-  } 
-}
-
-// function drawGradient(x, y) {
-//   var radius = dim/4;
-//   var h = random(0, 360);
-//   for (r = radius; r > 0; --r) {
-//     fill(h, 90, 90);
-//     ellipse(x, y, r, r);
-//     h = (h + 1) % 360;
-//   }
+let vertices = []; 
+let img;
+// function preload() {
+//   img = loadImage('prueba.png');
 // }
-
-function touchMoved() {
   
-  let object_width = 50;
-  let object_height = 50;
-  var h = random(0, 360);
+function setup() { 
+  createCanvas(displayWidth, displayHeight, SVG);
+  background(240);
+  
+} 
+  
+function mouseClicked() { 
+  // Update the vertices array with 
+  // current mouse position 
+  vertices.push({ x: mouseX, y: mouseY }); 
+  fill(40);
+  clear(); 
+  
+  // noFill(); 
+  
+  // Draw shape using the current vertices array 
+  beginShape(); 
+  for (let i = 0; i < vertices.length; i++) 
+  curveVertex(vertices[i].x, vertices[i].y); 
+    endShape(CLOSE); 
+  
+  fill("red"); 
+  // Draw a circle at all the vertices 
+  for (let i = 0; i < vertices.length; i++) 
+    circle(vertices[i].x, vertices[i].y, 15); 
+} 
 
-  ellipse(mouseX, mouseY, object_width, object_height);
-  fill(h, 90, 90);
-  // h = (h + 1) % 360;
-  // var h = random(0, 360);
-  return false;
+save_canvas = function() {
+save();
 }
-
-
